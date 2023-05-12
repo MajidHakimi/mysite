0eb3bf3236f81bcf4336005279093e6e1267d04d
majidhakimi <majid.hakimi@gmail.com>
2023-05-12 13:54:03 +0430

    finish ch01

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,14 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-# Create your models here.
 
+# class for manage published in quey set 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
+# Create your models here.
 class Post(models.Model):
 
     class Status(models.TextChoices):
@@ -19,24 +25,22 @@ class Post(models.Model):
                                related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
-    create = models.DateTimeField(auto_now_add=True)
-    update = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=20 ,
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
 
-
-
-
-    
+    objects = models.Manager() #defoult manager 
+    published = PublishedManager() #custom manager 
+  
     class Meta:
         # app_label = 'blog'
         ordering = ['-publish']
         indexes = [
             models.Index(fields=['-publish']),
-        ]
-        
-    def __str__(self) :
+        ]    
+    def __str__(self):
         return self.title
 
 
