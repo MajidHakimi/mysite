f1aa4a61196cf3912a5ce43c8781c562ef0fff10
majidhakimi <majid.hakimi@gmail.com>
2023-05-18 13:38:46 +0430

    02 update paginator for exception 1 - page out of rang 2 - pgae not integer

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+
 
 
 def post_list(request):
     post_list = Post.published.all()
 
     # add pagination 
-    # 1-we should get all post 
-    # 2-create pagination object and split post ( get all objects + split number )
     paginator = Paginator(post_list , 3)
-    # 3-get number of page pagination from user(request)
     page_number = request.GET.get('page',1)
-    # 4-save new post for each page pagination by get number_page
-    posts = paginator.page(page_number)
 
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    
 
     return render(request,
                  'blog/post/list.html',
