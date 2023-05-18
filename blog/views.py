b6a02ff06d9600ec573108188673eb7327b33a68
majidhakimi <majid.hakimi@gmail.com>
2023-05-18 12:19:53 +0430

    02 compalete pagnition functinality

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+
+    # add pagination 
+    # 1-we should get all post 
+    # 2-create pagination object and split post ( get all objects + split number )
+    paginator = Paginator(post_list , 3)
+    # 3-get number of page pagination from user(request)
+    page_number = request.GET.get('page',1)
+    # 4-save new post for each page pagination by get number_page
+    posts = paginator.page(page_number)
+
+
     return render(request,
                  'blog/post/list.html',
                  {'posts': posts})
